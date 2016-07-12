4184dc55a60af9e3d73bd4cebc21fb2c2dcf4e02
Jawadh-Salih <jawadhsalih.4th@gmail.com>
2016-07-12 09:34:02 +0530

    Code changed with some variables

--- KaggleNeuralNetwork.py
+++ KaggleNeuralNetwork.py
@@ -4,14 +4,21 @@ import numpy
 # fix random seed for reproducibility
 seed = 7
 numpy.random.seed(seed)
+
+
+filepathSalih = '/home/jawadhsr/Desktop/Kaggle/trani.csv'
+filepathRandika = '/home/rnavagamuwa/Documents/CSE/Semester7/DataMining/kaggle/weeklySeperatedData/week3.csv'
+
 # load pima indians dataset
-dataset = numpy.loadtxt("/home/rnavagamuwa/Documents/CSE/Semester7/DataMining/kaggle/weeklySeperatedData/week3.csv", delimiter=",")
+dataset = numpy.loadtxt(filepathRandika, delimiter=",")
 # split into input (X) and output (Y) variables
 X = dataset[:,0:10]
 Y = dataset[:,10]
 # create model
 model = Sequential()
 model.add(Dense(12, input_dim=10, init='uniform', activation='relu'))
+
+# given that activation function as 'softplus' as it's range is (0,inf)
 model.add(Dense(8, init='uniform', activation='softplus'))
 model.add(Dense(1, init='uniform', activation='softplus'))
 # Compile model
