694bfc8018b7a112025ccb226bb4218345e79d14
prashan <nipunaprashan99@gmail.com>
2016-07-13 18:56:05 +0530

    Fixed key ordering issue & removed unnecessary rows

--- MedianRejectionSampling.py
+++ MedianRejectionSampling.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
+import time
 from collections import defaultdict
 from sklearn.base import BaseEstimator
 from random import random
 from random import randint
+
 
 ########################################
 # Median Class
@@ -16,11 +18,12 @@ class Median(BaseEstimator):
         self.not_found = 0
 
     def fit(self, X, y=None):
-        block = pd.concat([X, y], axis=1)  #concatination of both x and y blocks into one block
-        target = block.columns[-1]   # Demanda_uni_equil is our target (we want to calculate demand)  target = Demanda_uni_equil
-
+        block = pd.concat([X, y], axis=1)  # concatination of both x and y blocks into one block
+        target = block.columns[
+            -1]  # Demanda_uni_equil is our target (we want to calculate demand)  target = Demanda_uni_equil
         # all keys dict
         all_keys = block.groupby(self.keys).agg({target: np.median})
+
         # initalise dict list
         self.dicts = [all_keys.to_dict()[target]]  # here map has been creaated for given keys (A,B,C -> demand)
 
@@ -34,37 +37,23 @@ class Median(BaseEstimator):
         self.dicts.append(defaultdict(lambda: global_median))
         return self
 
-    def get_value2(self, keys, dicts):
+    # get mapped trained value for test data
+    def fetch_value_from_trained_dic(self, keys, dicts):
         tkey = tuple(keys)
         # if tkey in dicts[0].keys():
         try:
             return dicts[0][tkey]
         # else:
         except KeyError:
-            return self.get_value2(keys[:-1], dicts[1:])
-
-    def get_value(self, key):
-        print("d")
-        self.total += 1
-        key = tuple(key)
-        try:
-            return self.product_median2_dict[key]
-        except KeyError:
-            try:
-                return self.product_median_dict[key[0]]
-            except KeyError:
-                self.not_found += 1
-                return self.global_median
+            return self.fetch_value_from_trained_dic(keys[:-1], dicts[1:])
 
     def predict(self, X):
-        # return X[self.keys].map(lambda t: self.get_value(t))
-        # return X[self.keys].apply(lambda t: self.get_value(t), axis=1)
-        return X[self.keys].apply(lambda t: self.get_value2(t, self.dicts), axis=1)
-
+        return X[self.keys].apply(lambda t: self.fetch_value_from_trained_dic(t, self.dicts),
+                                  axis=1)  # get key columns and check with train data
 
 
 ######################################
-#rejection Sampling class
+# rejection Sampling class
 ######################################
 
 class RejectionSampling(object):
@@ -77,18 +66,16 @@ class RejectionSampling(object):
         n = 0.0
         for x in self.iterator:
             n += 1.0
-            r = random() # returns numbers between 0 and 1
+            r = random()  # returns numbers between 0 and 1
             if r < (self.k / n):
-                self.__accept(x,n)
+                self.__accept(x, n)
         return self.ans
 
-    def __accept(self, x,n):
+    def __accept(self, x, n):
         if len(self.ans) == self.k:
             idx = randint(0, self.k - 1)
             del self.ans[idx]
         self.ans.append(x)
-
-
 
 
 def get_data(N, quick=False):
@@ -98,7 +85,8 @@ def get_data(N, quick=False):
     if quick:
         df_train = pd.read_csv('/home/prashan/Desktop/DM/Kaggle/data/train.csv', usecols=columns, nrows=40000)
     else:
-        data = pd.read_csv('/home/prashan/Desktop/DM/Kaggle/data/train.csv', usecols=columns, iterator=True, chunksize=chunk)
+        data = pd.read_csv('/home/prashan/Desktop/DM/Kaggle/data/train.csv', usecols=columns, iterator=True,
+                           chunksize=chunk)
         chunks = RejectionSampling(data, N).run()
         df_train = pd.concat(chunks, ignore_index=True)
 
@@ -109,35 +97,20 @@ def get_data(N, quick=False):
 
 def submit(estimator, cols):
     # cols = ['Agencia_ID','Canal_ID','Ruta_SAK','Cliente_ID','Producto_ID']
-    df_test = pd.read_csv('/home/prashan/Desktop/DM/Kaggle/data/test.csv', usecols=cols)
+    df_test = pd.read_csv('/home/prashan/Desktop/DM/Kaggle/data/test.csv')
     sub = pd.read_csv('/home/prashan/Desktop/DM/Kaggle/data/sample_submission.csv')
     sub['Demanda_uni_equil'] = estimator.predict(df_test)
-    sub.to_csv('/home/prashan/Desktop/DM/Kaggle/data/result.csv', index=False)
+    sub.to_csv('/home/prashan/Desktop/DM/Kaggle/data/result2.csv', index=False)
 
 
 if __name__ == "__main__":
-    # N = 8000  # 2
-    # N = 4000  # 3
-    # N = 1650
-    # N = 10
-    # N = 800
     N = 8000
     # [X,y] = get_data(N, quick=True)
     [X, y] = get_data(N)
     # print(X.shape)  # this comes from pandas - prints no of rows and cols
 
-
-    # key = 'Producto_ID'
-    # key = ['Producto_ID', 'Agencia_ID']
-    # keys = ['Canal_ID', 'Producto_ID', 'Cliente_ID']
-    # keys = ['Canal_ID', 'Producto_ID', 'Cliente_ID', 'Agencia_ID']
-    # keys = ['Producto_ID']
-    # keys = ['Producto_ID', 'Canal_ID']
-    # keys = ['Producto_ID', 'Canal_ID', 'Cliente_ID', 'Ruta_SAK']
-    keys = ['Producto_ID', 'Canal_ID','Ruta_SAK', 'Ruta_SAK']
-
+    keys = ['Canal_ID', 'Ruta_SAK', 'Producto_ID', 'Cliente_ID']
 
     clf = Median(keys)
-    clf.fit(X,y)
+    clf.fit(X, y)
     submit(clf, keys)
-
