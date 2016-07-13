e30bba05746ceff3a4dbc1e6e618200c3311d3f5
rnavagamuwa <randikanavagamuwa@gmail.com>
2016-07-13 14:51:21 +0530

    data seperation mechanism updated

--- NeuralNetwork/SeperateRelevantData.py
+++ NeuralNetwork/SeperateRelevantData.py
@@ -1,12 +1,28 @@
-pathToCompleteDataSet = "/home/rnavagamuwa/Documents/CSE/Semester7/DataMining/kaggle/train.csv"
-pathToRquiredInputtDataSet = "/home/rnavagamuwa/Documents/CSE/Semester7/DataMining/kaggle/RelevantDataSet/requiredInput.csv"
-pathToOutPutDataSet = "/home/rnavagamuwa/Documents/CSE/Semester7/DataMining/kaggle/RelevantDataSet/outPutData.csv"
+pathToCompleteDataSet = "/home/rnavagamuwa/Documents/CSE/Semester7/DataMining/kaggle/weeklySeperatedData/week4.csv"
+pathToRquiredInputtDataSet = "/home/rnavagamuwa/Documents/CSE/Semester7/DataMining/kaggle/weeklySeperatedData/week4Relevant.csv"
+
 
 
 completeDataSet = open(pathToCompleteDataSet,"r")
 requiredDataSet = open(pathToRquiredInputtDataSet,"w")
-outPutDataSet = open(pathToOutPutDataSet,"w")
 
+
+# Input and output in seperate files
+# pathToOutPutDataSet = "/home/rnavagamuwa/Documents/CSE/Semester7/DataMining/kaggle/weeklySeperatedData/outPutData.csv"
+# outPutDataSet = open(pathToOutPutDataSet,"w")
+# for line in completeDataSet:
+#     if line.startswith("S"):
+#         continue
+#     row = line.split(",")
+#     requiredDataSet.write(row[2]+",")
+#     requiredDataSet.write(row[3]+",")
+#     requiredDataSet.write(row[4]+",")
+#     requiredDataSet.write(row[5]+"\n")
+#     outPutDataSet.write(row[10])
+#
+
+
+# input and output in the same file
 for line in completeDataSet:
     if line.startswith("S"):
         continue
@@ -14,5 +30,5 @@ for line in completeDataSet:
     requiredDataSet.write(row[2]+",")
     requiredDataSet.write(row[3]+",")
     requiredDataSet.write(row[4]+",")
-    requiredDataSet.write(row[5]+"\n")
-    outPutDataSet.write(row[10])
+    requiredDataSet.write(row[5]+",")
+    requiredDataSet.write(row[10])
