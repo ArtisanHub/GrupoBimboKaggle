96f61e5f8943c46f33714214a8cf66f69a3327e2
PravindaPerera <pravinda.joes@gmail.com>
2016-08-30 19:55:49 +0530

    modified rejectio sampling

--- Sampling/BestMatchSampling.py
+++ Sampling/BestMatchSampling.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 from sklearn.base import BaseEstimator
 from random import random
 from random import randint
+from sklearn import preprocessing
 
 
 ########################################
@@ -20,23 +21,47 @@ class Median(BaseEstimator):
     def fit(self, X, y=None):
         block = pd.concat([X, y], axis=1)  # concatination of both x and y blocks into one block
         target = block.columns[-1]  # Demanda_uni_equil is our target
-        # all keys dict
 
-        all_keys = block.groupby(self.keys).agg({target: np.median})
-        print(all_keys)
+        # all_keys = block.groupby(self.keys).agg({target: np.median})
+        all_keys = block.groupby(self.keys)['Demanda_uni_equil'].apply(lambda x: self.get_bestmatch_value(x))
 
-        # initalise dict list
-        self.dicts = [all_keys.to_dict()[target]]  # here map has been creaated for given keys (A,B,C -> demand)
+        # self.dicts = [all_keys.to_dict()[target]]  # here map has been creaated for given keys (A,B,C -> demand)
+        self.dicts = [all_keys.to_dict()]
 
         # all other dicts ( There can be other combinations as well,like A,B, A)
         for n in range(1, len(self.keys)):
             keys = self.keys[:-n]
-            key_grouped = block.groupby(keys).agg({target: np.mean})
-            self.dicts.append(key_grouped.to_dict()[target])
+            # key_grouped = block.groupby(keys).agg({target: np.mean})
+            key_grouped = block.groupby(keys)['Demanda_uni_equil'].apply(lambda x: self.get_bestmatch_value(x))
+            # self.dicts.append(key_grouped.to_dict()[target])
+            self.dicts.append(key_grouped.to_dict())
 
         global_median = np.mean(y)
         self.dicts.append(defaultdict(lambda: global_median))
         return self
+
+    def get_bestmatch_value(self, keys):
+        array = tuple(keys)
+        array_length = len(array)
+        stdCoefficient =1
+
+        std = np.std(array)
+        mean = np.mean(array)
+
+        array = preprocessing.scale(array)
+
+        lowerRange = np.abs(stdCoefficient * std - mean)
+        upperRange = np.abs(stdCoefficient * std + mean)
+
+        i = 0
+        newList = list()
+        while i < array_length:
+            if lowerRange <= array[i] <= upperRange:
+                array[i] = (array[i]*std) + mean
+                newList.append(array[i])
+            i += 1
+
+        return np.median(newList)
 
     # get mapped trained value for test data
     def fetch_value_from_trained_dic(self, keys, dicts):
@@ -80,16 +105,15 @@ class RejectionSampling(object):
 
 
 def get_data(N, quick=False):
-    chunk = 10 ** 4  # 10^3 = 1000
+    chunk = 10 ** 4
     columns = ['Semana', 'Agencia_ID', 'Canal_ID', 'Ruta_SAK', 'Cliente_ID', 'Producto_ID', 'Demanda_uni_equil']
 
     if quick:
         data = pd.read_csv('D:/FYP-Developments/Dataset-Kaggale/MedianRejectionSamplingData/train.csv', usecols=columns, nrows=40000)
     else:
-
-        data = pd.read_csv('/home/prashan/Desktop/DM/Kaggle/data/train.csv', usecols=columns)
-        #chunks = RejectionSampling(data, N).run()
-        #df_train = pd.concat(chunks, ignore_index=True)
+        data = pd.read_csv('D:/FYP-Developments/Dataset-Kaggale/MedianRejectionSamplingData/train.csv', usecols=columns)
+        # chunks = RejectionSampling(data, N).run()
+        # df_train = pd.concat(chunks, ignore_index=True)
 
     X = data[['Semana', 'Agencia_ID', 'Canal_ID', 'Ruta_SAK', 'Cliente_ID', 'Producto_ID']]  # .values
     y = data['Demanda_uni_equil']
@@ -98,14 +122,14 @@ def get_data(N, quick=False):
 
 def submit(estimator, cols):
     # cols = ['Agencia_ID','Canal_ID','Ruta_SAK','Cliente_ID','Producto_ID']
-    df_test = pd.read_csv('/home/prashan/Desktop/DM/Kaggle/data/test.csv')
-    sub = pd.read_csv('/home/prashan/Desktop/DM/Kaggle/data/sample_submission.csv')
+    df_test = pd.read_csv('D:/FYP-Developments/Dataset-Kaggale/MedianRejectionSamplingData/test.csv')
+    sub = pd.read_csv('D:/FYP-Developments/Dataset-Kaggale/MedianRejectionSamplingData/sample_submission.csv')
     sub['Demanda_uni_equil'] = estimator.predict(df_test)
-    sub.to_csv('/home/prashan/Desktop/DM/Kaggle/data/result2.csv', index=False)
+    sub.to_csv('D:/FYP-Developments/Dataset-Kaggale/MedianRejectionSamplingData/bestmatchresult.csv', index=False)
 
 
 if __name__ == "__main__":
-    N = 8000
+    N = 1000
     # [X,y] = get_data(N, quick=True)
     [X, y] = get_data(N)
     # print(X.shape)  # this comes from pandas - prints no of rows and cols
@@ -116,4 +140,7 @@ if __name__ == "__main__":
 
     clf = Median(keys)
     clf.fit(X, y)
+    print("Fitting")
     submit(clf, keys)
+
+    print("done")
